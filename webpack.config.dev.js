8d2bc774e0e33666e0f04910173fef2826af93b0
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-02-25 20:50:50 +0000

    chore: setup webpack config (#1)

---
+++ webpack.config.dev.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/index',
+        './index.html'
+    ],
+    mode: 'development',
+    devtool: 'cheap-module-source-map',
+    watch: true,
+    output: {
+        filename: './bundle.js',
+    },
+    devServer: {
+        hot: true,
+        inline: true,
+        host: "localhost",
+        port: 8080,
+        watchOptions: {
+            poll: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    { loader: 'css-loader' },
+                    {loader: 'postcss-loader'}
+                ],
+            },
+            { test: /\.html$/, loader: 'html-loader' },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './index.html',
+            filename: 'index.html',
+            minify: false
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
