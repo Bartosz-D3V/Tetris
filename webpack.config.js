6ed398ef66a95e1b95deb17a8448d1f81dae9f94
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-02-26 07:29:57 +0000

    fix: eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+/* eslint-disable global-require, import/no-dynamic-require */
 const buildConfig = env => {
-    if (env === 'dev' || env === 'prod') {
-        return require(`./webpack.config.${env}.js`);
-    } else {
-        throw new ReferenceError('Wrong webpack build parameter. Possible choices: `dev` or `prod`.');
-    }
+  if (env === "dev" || env === "prod") {
+    return require(`./webpack.config.${env}.js`);
+  }
+  throw new ReferenceError(
+    "Wrong webpack build parameter. Possible choices: `dev` or `prod`."
+  );
 };
 
 module.exports = buildConfig;
